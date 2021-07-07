aef9814c1628898a8a4be5c6d521d19bfb8c06f4
Domingo <dominlopez@uma.es>
2021-07-07 19:13:29 +0200

    Better tikz engine

--- R/rlatex_engine.R
+++ R/rlatex_engine.R
@@ -44,22 +44,57 @@ rtikz <- function(options) {
 
     filename <- file.path(options$fig.path,
                           paste0(options$label, ".tex"))
+    options( tikzLatexPackages = c(
+      "\\usepackage{tikz}",
+      "\\usepackage[active,tightpage,psfixbb]{preview}",
+      "\\PreviewEnvironment{pgfpicture}",
+      "\\setlength\\PreviewBorder{0pt}",
+      # getOption( "tikzLatexPackages" ),
+      "\\usepackage{amssymb}"
+    ))
 
     tikzDevice::tikz(filename,
+                     standAlone = knitr::is_html_output(),
                      width = options$fig.width,
                      height = options$fig.height)
     print(eval(parse(text = the_code)))
     invisible(grDevices::dev.off())
 
-    out <- paste0("\\input{",
-                  filename,
-                  "}")
+    if (knitr::is_html_output()) {
 
-    options$echo <- FALSE
-    options$results <- "asis"
-    knitr::engine_output(options,
-                         options$code,
-                         out)
+      outfile <- tinytex::pdflatex(filename)
+
+      bitmap <- pdftools::pdf_render_page(outfile,
+                                page = 1,
+                                dpi = 300)
+      png::writePNG(bitmap, paste0(outfile, ".png"))
+
+      # pdftools::pdf_convert(outfile, format = "png",
+      #                       dpi = 300, verbose = FALSE)
+      outfile <- paste0(outfile, ".png")
+      # outfile <- knitr::include_graphics(outfile)
+      out <- paste0("![](", outfile, ")")
+      #
+      options$echo <- FALSE
+      options$results <- "asis"
+      knitr::engine_output(options,
+                           options$code,
+                           out)
+
+    } else {
+
+      out <- paste0("\\input{",
+                    filename,
+                    "}")
+
+      options$echo <- FALSE
+      options$results <- "asis"
+      knitr::engine_output(options,
+                           options$code,
+                           out)
+
+
+    }
 
   }
 
