4c5eed866ca0bc6ddb85e4e2564651d010815529
FPG1993 <franciscoperezgamez@uma.es>
2021-07-20 13:39:53 +0200

    actualizar norma

--- R/norma.R
+++ R/norma.R
@@ -1,10 +1,12 @@
+#' @export
 norma<-function(v,p){
-  if p=1 {
+  if (p==1) {
+
     return (sum(abs(v)))
 
-  }
-  else
+  }if (p==Inf) {
+    return(max(abs(v)))
+      }else{
     return("aun no definida")
-
-
+}
 }
